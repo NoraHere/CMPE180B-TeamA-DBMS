d24ea9cf44373aadcbc39311fb677714be1dec98
JY <e1237248@u.nus.edu>
2025-12-06 18:55:53 -0800

    add sql/

---
+++ sql/Category.sql
@@ -0,0 +1,4 @@
+INSERT INTO Category VALUES ("1","Reviewed");
+INSERT INTO Category VALUES ("2","Unreviewed");
+INSERT INTO Category VALUES ("3","Survey");
+INSERT INTO Category VALUES ("4","Conference");
